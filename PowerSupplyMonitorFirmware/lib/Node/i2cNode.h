bf9b4cd72a6d059614a020edb368854fe1ee5163
Elektrospy <15101742+Elektrospy@users.noreply.github.com`>
2019-03-04 20:44:04 +0100

    removed external tca library

--- PowerSupplyMonitorFirmware/lib/Node/i2cNode.h
+++ PowerSupplyMonitorFirmware/lib/Node/i2cNode.h
@@ -1,34 +1,29 @@
 // i2cnode.h
 #ifndef I2CNODE_H
 #define I2CNODE_H
-#include <Wire.h>
-#include <TwoWireMux.h>
-#include <TI_TCA9548A.h>
 #include <Adafruit_ADS1015.h>
 
 class i2cNode {
 	
 public:
 	i2cNode();
-	i2cNode(TI_TCA9548A *tca, uint8_t tcaPort);
+	i2cNode(uint8_t tcaPort);
 	~i2cNode();
-	float getAmpereForOutput(uint8_t);
+	uint8_t getTcaPort();
 	uint16_t getRawInput(uint8_t);
+	float getAmpereForOutput(uint8_t);
+	uint16_t getMilliAmpereForOutput(uint8_t);
 
 private:
 	uint16_t _numberOfProbes;
-	uint16_t _adcListRaw[4];
-	uint16_t _adcListAmpere[4];
 	uint8_t _numberOfAdcs;
 	uint8_t _tcaPort;
 	Adafruit_ADS1015 _ads;
-	TI_TCA9548A *_tca;
 
 	void _init();
-
-	void _updateRawValues();
 	uint16_t _getRaw(uint8_t);
 	float _calculateAmpereFromRaw(uint16_t);
+	uint16_t _calculateMilliAmpereFromRaw(uint16_t);
 };
 
 #endif
