269f0c9ca9e1e7cd018b73429e1a182bc8e81b4e
Elektrospy <15101742+Elektrospy@users.noreply.github.com>
2019-03-08 14:46:44 +0100

    added branch for testing

--- PowerSupplyMonitorFirmware/src/PowerSupplyMonitorFirmware.ino
+++ PowerSupplyMonitorFirmware/src/PowerSupplyMonitorFirmware.ino
@@ -5,20 +5,21 @@
 // ESP8266, SDA: D1, SCL: D2
 #define PIN_SDA D1
 #define PIN_SCL D2
-// debug print cycle counter
+const unsigned long serialBautrate = 115200;
+const uint32_t i2cClockSpeed = 400000;
 int cycleCount = 0;
-// create i2cHub object, holding our i2cNodes
 i2cHub nodeHub = i2cHub();
+
 // prototype methods
 void debugNodePrint();
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(serialBautrate);
     // init i2c wire bus
     Wire.begin(PIN_SDA, PIN_SCL);
     // Supported baud rates are 100kHz, 400kHz, and 1000kHz
-    Wire.setClock(400000);
-    // init nodeHub
+    Wire.setClock(i2cClockSpeed);
+    // init nodeHub for i2c usage
     nodeHub.init();
 }
 
@@ -31,19 +32,13 @@ void debugNodePrint() {
     const int numberOfNodes = nodeHub.getNodeCount();
     cycleCount++;
     for(uint8_t currentNode=0; currentNode < numberOfNodes; currentNode++) {
-        Serial.print("Node #");
-        Serial.print(currentNode);
+        Serial.printf("Node #%d", currentNode);
         const int adsChannels = nodeHub.getNodeChannelCount(currentNode);
         for(uint8_t currentChannel=0; currentChannel < adsChannels; currentChannel++) {
-            Serial.print(" Ch");
-            Serial.print(currentChannel);
-            Serial.print(":");
-            //Serial.print(i2cNodeList[currentNode].getRawInput(currentChannel));
-            Serial.print(nodeHub.getMilliAmpereForNode(currentNode, currentChannel));
+            Serial.printf(" Ch%d:%f", currentChannel, nodeHub.getMilliAmpereForNode(currentNode, currentChannel));
         }
         Serial.println("");
     }
-    Serial.print("===============:");
-    Serial.println(cycleCount);
+    Serial.printf("===============:%d\n", cycleCount);
     delay(500);
 }
